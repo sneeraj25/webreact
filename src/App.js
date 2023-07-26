6b9afc8564d49c9e714f8b89cfda0a3011bd91bc
sneeraj25 <shubham.beechtreeitsolutions@gmail.com>
2023-07-26 15:33:23 +0530

    my second coomit reactweb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useDispatch ,useSelector } from 'react-redux';
+import { incNumber,decNumber } from './actions/index';
 
-function App() {
+const App = () => {
+
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <section className='App App-header'>
+          <div className='container'>
+            
+              <div className='head'>
+                <h1>Increment / Decrement Counter</h1>
+                <p>Using React and Redux</p>
+              </div>
+            <div className=''>
+                
+                <input type='text' className='form-control col-md-3 mx-auto' name='quantity' value = {myState}  />
+                <a onClick={ () => dispatch(decNumber()) } title='Increment' className='btn mx-1 px-4 btn-primary' >
+                  <span>-</span>
+                </a>
+                <a onClick={ () => dispatch(incNumber()) } title='Decrement' className='btn mx-1 px-4 btn-primary' >
+                  <span>+</span>
+                </a>
+              </div>
+          </div>
+        </section>
+    </>
+  )
 }
 
-export default App;
+export default App
